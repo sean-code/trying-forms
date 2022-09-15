3f1da56938e019afd73d8afa0647f878ce9a1b9d
sean-code <john.nganga@student.moringaschool.com>
2022-09-15 13:51:27 +0300

    create and style form

---
+++ index.js
@@ -0,0 +1,8 @@
+// const add = document.createElement('p');
+
+document.getElementById('Log-in').innerHTML = 'Enter Your Log In Credentials'
+
+let content = document.querySelector('#Log-in');
+content.style.textAlign = 'center'
+content.style.color = 'indigo'
+content.style.backgroundColor = 'brown'
