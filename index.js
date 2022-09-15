405b7ed28ba98f63edd8fd4e958eeec0338eca4b
sean-code <john.nganga@student.moringaschool.com>
2022-09-15 14:21:32 +0300

    Add event listener

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ content.style.fontSize = 'xx-large'
 content.style.backgroundColor = 'brown'
 content.style.margin= '3% 10% 1% 10%'
 content.style.padding = '2%'
+
+const button = document.getElementById('btn')
+button.addEventListener('submit', (event)=>{
+    event.preventDefault();
+})
